cfef55f8b305a6b5d68a477e92aa96c11e564618
Chris Rhodes <iopred@gmail.com>
2015-12-10 01:18:41 -0800

    Tabs to spaces

--- discord.go
+++ discord.go
@@ -1,73 +1,73 @@
 package main
 
 import (
-	"strconv"
+  "strconv"
 
-	"github.com/Xackery/discord"
+  "github.com/Xackery/discord"
 )
 
 type DiscordMessage discord.Message
 
 func (m *DiscordMessage) Channel() string {
-	return strconv.Itoa(m.ChannelID)
+  return strconv.Itoa(m.ChannelID)
 }
 
 func (m *DiscordMessage) User() string {
-	return m.Author.Username
+  return m.Author.Username
 }
 
 func (m *DiscordMessage) Message() string {
-	return m.Content
+  return m.Content
 }
 
 type Discord struct {
-	Client      *discord.Client
-	MessageChan chan Message
+  Client      *discord.Client
+  MessageChan chan Message
 }
 
 func NewDiscord() *Discord {
-	return &Discord{
-		Client:      &discord.Client{},
-		MessageChan: make(chan Message, 200),
-	}
+  return &Discord{
+    Client:      &discord.Client{},
+    MessageChan: make(chan Message, 200),
+  }
 }
 
 func (d *Discord) onMessage(event discord.Event, message discord.Message) {
-	dm := DiscordMessage(message)
-	if message.Author.ID != d.Client.User.ID {
-		d.MessageChan <- &dm
-	}
+  dm := DiscordMessage(message)
+  if message.Author.ID != d.Client.User.ID {
+    d.MessageChan <- &dm
+  }
 }
 
 func (d *Discord) Name() string {
-	return "Discord"
+  return "Discord"
 }
 
 func (d *Discord) MessageChannel() chan Message {
-	return d.MessageChan
+  return d.MessageChan
 }
 
 func (d *Discord) Open() error {
 
-	if err := d.Client.Login("iopred+bot@gmail.com", "dgmrthe1"); err != nil {
-		return err
-	}
+  if err := d.Client.Login("iopred+bot@gmail.com", "dgmrthe1"); err != nil {
+    return err
+  }
 
-	d.Client.OnMessageCreate = d.onMessage
+  d.Client.OnMessageCreate = d.onMessage
 
-	return d.Client.Listen()
+  return d.Client.Listen()
 }
 
 func (d *Discord) Send(channel, message string) error {
-	id, err := strconv.Atoi(channel)
-	if err != nil {
-		return err
-	}
+  id, err := strconv.Atoi(channel)
+  if err != nil {
+    return err
+  }
 
-	_, err = d.Client.ChannelMessageSend(id, message)
-	if err != nil {
-		return err
-	}
+  _, err = d.Client.ChannelMessageSend(id, message)
+  if err != nil {
+    return err
+  }
 
-	return nil
+  return nil
 }
--- interfaces.go
+++ interfaces.go
@@ -1,14 +1,14 @@
 package main
 
 type Message interface {
-	Channel() string
-	User() string
-	Message() string
+  Channel() string
+  User() string
+  Message() string
 }
 
 type Service interface {
-	Name() string
-	MessageChannel() chan Message
-	Open() error
-	Send(channel, message string) error
+  Name() string
+  MessageChannel() chan Message
+  Open() error
+  Send(channel, message string) error
 }
