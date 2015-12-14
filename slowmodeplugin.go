67f5f252727762e2a9d9d981d281e95e1d09d834
Chris Rhodes <iopred@gmail.com>
2015-12-13 23:40:27 -0800

    Cache isModerator

--- slowmodeplugin.go
+++ slowmodeplugin.go
@@ -27,20 +27,21 @@ func (e *SlowModePlugin) Register(bot *Bot, service Service, data []byte) error 
       message := <-messageChannel
       messageMessage := strings.Replace(message.Message(), " ", "", -1)
       messageChannel := message.Channel()
+      messageModerator := message.IsModerator()
 
-      if messageMessage == "!slowmodeon" && message.IsModerator() {
+      if messageMessage == "!slowmodeon" && messageModerator {
         e.Enabled[messageChannel] = true
         service.SendMessage(messageChannel, "Slow mode is now on (You will be temporarily banned for 30 seconds when you chat).")
-      } else if messageMessage == "!slowmodeoff" && message.IsModerator() {
+      } else if messageMessage == "!slowmodeoff" && messageModerator {
         e.Enabled[messageChannel] = false
         service.SendMessage(messageChannel, "Slow mode is now off.")
-      } else if messageMessage == "!slowmode" && message.IsModerator() {
+      } else if messageMessage == "!slowmode" && messageModerator {
         if e.Enabled[messageChannel] {
           service.SendMessage(messageChannel, "Slow mode is on.")
         } else {
           service.SendMessage(messageChannel, "Slow mode is off.")
         }
-      } else if e.Enabled[messageChannel] && !message.IsModerator() {
+      } else if e.Enabled[messageChannel] && !messageModerator {
         if err := service.BanUser(messageChannel, message.UserId(), 30); err != nil {
           log.Println(err)
         }
