964a57270717f53f7f300244012a61273c7981c0
Chris Rhodes <iopred@gmail.com>
2015-12-17 09:04:47 -0800

    Temporarily remove Moderator actions as they are not supported yet.

--- youtube.go
+++ youtube.go
@@ -295,8 +295,6 @@ func (yt *YouTube) Open() (<-chan Message, error) {
           insertLiveChatMessageLimited(request)
         case *ytc.LiveChatBan:
           yt.Client.InsertLiveChatBan(request)
-        case *ytc.LiveChatModerator:
-          yt.Client.InsertLiveChatModerator(request)
         }
       case request := <-yt.DeleteChan:
         switch request := request.(type) {
@@ -304,8 +302,6 @@ func (yt *YouTube) Open() (<-chan Message, error) {
           yt.Client.DeleteLiveChatMessage(request)
         case *ytc.LiveChatBan:
           yt.Client.DeleteLiveChatBan(request)
-        case *ytc.LiveChatModerator:
-          yt.Client.DeleteLiveChatModerator(request)
         }
       }
 
